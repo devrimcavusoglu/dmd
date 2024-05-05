98a353b0df2a336a889c1c2979d1457620114013
devrimcavusoglu <devrimcavusoglu@gmail.com>
2024-05-05 21:29:43 +0300

    - EDM and DMD models are added to download_data.sh.

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
 mkdir -p data
+mkdir -p models
 
 # Download Distillation dataset in HDF5 format
 wget https://huggingface.co/datasets/Devrim/dmd_cifar10_edm_distillation_dataset/resolve/main/cifar.hdf5?download=true -O data/cifar.hdf5
 wget https://huggingface.co/datasets/Devrim/dmd_cifar10_edm_distillation_dataset/resolve/main/cifar_toy.hdf5?download=true -O data/cifar_toy.hdf5
 
+wget https://huggingface.co/Devrim/dmd-cifar-10-cond/resolve/main/model.pt?download=true -O models/dmd_cifar_10_cond.pt
 
 # Download vanilla zip dataset
 # wget https://huggingface.co/datasets/Devrim/dmd_cifar10_edm_distillation_dataset/resolve/main/distillation_dataset.zip?download=true -O data/distillation_dataset.zip
 #cd data
 #unzip -oq distillation_dataset.zip
 #rm -rf distillation_dataset.zip
+
+# By default the pipeline automatically downloads given URL, but to download the base model (EDM) to locale comment off the following line
+# wget https://nvlabs-fi-cdn.nvidia.com/edm/pretrained/edm-cifar10-32x32-cond-vp.pkl -O models/edm-cifar10-32x32-cond-vp.pt
